88e0d0e2a55e4859be2a2c96b9abfe8786630f23
Anoop2691 <40260366+Anoop2691@users.noreply.github.com>
2018-06-18 08:34:25 +0530

    Update tatoc1.java

--- tatoc1.java
+++ tatoc1.java
@@ -1,4 +1,4 @@
-package Auto;
+package automationFramework;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
@@ -6,40 +6,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class Box {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kumar\\Desktop\\selenium jar files\\chromedriver.exe");
+		   System.setProperty("webdriver.chrome.driver", "C:\\Users\\anoopkumar\\Downloads\\chromedriver.exe");
 		   WebDriver driver = new ChromeDriver();
-
            System.out.println("Chrome is opened");
-
            driver.get("http://10.0.1.86/tatoc/basic/grid/gate");
-
            System.out.println("tatoc is selected");
-
-//Find the text input element by its name
-
-          WebElement element = driver.findElement(By.className("greenbox"));
-//Enter something to search                 
-          element.click();
-          String parentFrameDivClass="",childFrameDivClass="";
+           driver.findElement(By.className("greenbox")).click();          String parentFrameDivClass="",childFrameDivClass="";
           parentFrameDivClass=driver.switchTo().frame("main").findElement(By.id("answer")).getAttribute("class");
-          //System.out.println(s);
           do {
         	  childFrameDivClass=driver.switchTo().frame("child").findElement(By.id("answer")).getAttribute("class");;
-          //System.out.println(s1);
-          driver.switchTo().parentFrame();
-          if(parentFrameDivClass.equals(childFrameDivClass)) {
-        	  driver.findElement(By.linkText("Proceed")).click();
-          }
-          else {
-        	  driver.findElement(By.linkText("Repaint Box 2")).click();
-          }
+        	  driver.switchTo().parentFrame();
+        	  if(parentFrameDivClass.equals(childFrameDivClass)) {
+        		  driver.findElement(By.linkText("Proceed")).click();
+        	  }
+        	  else {
+        		  driver.findElement(By.linkText("Repaint Box 2")).click();
+        	  }
           }while(!parentFrameDivClass.equals(childFrameDivClass));
           new Actions(driver).dragAndDrop(driver.findElement(By.id("dragbox")),driver.findElement(By.id("dropbox"))).perform();
           driver.findElement(By.linkText("Proceed")).click();
@@ -58,13 +47,7 @@ public class Box {
           String[] str=c.split(" ");
           Cookie ck=new Cookie("Token", str[str.length-1]);
           driver.manage().addCookie(ck);
-          //Thread.sleep(2000);
          driver.findElement(By.linkText("Proceed")).click();
-       // Continue with original browser (first window)
-
-
-          
+		driver.close();
 	}
-
-
 }
